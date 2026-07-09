cf446a19219433ae43035d887db542f3a2c0be9d
ImSH715 <seunghyunim2@gmail.com>
2026-07-09 14:45:06 +0100

    switch to cpu

--- models/deep_learning/self_model.sh
+++ models/deep_learning/self_model.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=simclr_har
-#SBATCH --partition=cpu       
+#SBATCH --partition=sheffield      
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
 #SBATCH --time=24:00:00      
